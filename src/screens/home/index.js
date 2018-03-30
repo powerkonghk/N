1ac26c25bd0812ae7247eae9f7b93c3b6eb8fb8a
Sunny Lau <powerkonghk@gmail.com>
2018-03-30 10:38:15 +0800

    add face id support

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,14 +1,34 @@
 import React, { Component } from "react";
-import { ImageBackground, View, StatusBar } from "react-native";
+import { ImageBackground, View, StatusBar, AlertIOS } from "react-native";
 import { Container, Button, H3, Text } from "native-base";
-
+import { TouchableHighlight } from 'react-native'
+import TouchID from 'react-native-touch-id'
 import styles from "./styles";
 
 const launchscreenBg = require("../../../assets/launchscreen-bg.png");
 const launchscreenLogo = require("../../../assets/logo-kitchen-sink.png");
 
+const optionalConfigObject = {
+  title: "Authentication Required",
+  color: "#e00606"
+}
+
 class Home extends Component {
+
+  _pressHandler() {
+    TouchID.authenticate('to demo this react-native component', optionalConfigObject)
+      .then(success => {
+        AlertIOS.alert('Authenticated Successfully');
+        this.props.navigation.navigate("DrawerOpen");
+      })
+      .catch(error => {
+        console.log(error);
+        AlertIOS.alert('Authentication Failed');
+      });
+  }
+
   render() {
+
     return (
       <Container>
         <StatusBar barStyle="light-content" />
@@ -26,12 +46,22 @@ class Home extends Component {
             <H3 style={styles.text}>CallMyIT</H3>
             <View style={{ marginTop: 8 }} />
             <H3 style={styles.text}>Hospital Authority ITO All rights reserved</H3>
-            <View style={{ marginTop: 8 }} />
+            <View style={{ marginTop: 8 }} >
+
+              <TouchableHighlight navigation={this.props.navigation} onPress={this._pressHandler}>
+
+                <Text>
+                  Authenticate with Touch ID
+                  </Text>
+
+              </TouchableHighlight>
+
+            </View>
           </View>
           <View style={{ marginBottom: 80 }}>
             <Button
               style={{ backgroundColor: "#6FAF98", alignSelf: "center" }}
-              onPress={() => this.props.navigation.navigate("DrawerOpen")}
+              onPress={this._pressHandler}
             >
               <Text>Lets Go!</Text>
             </Button>
